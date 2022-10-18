b672e087dc3b59da7000df8bb7bc87523600ce77
louisomo <aisagbonhilouis@gmail.com>
2022-10-18 02:58:28 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const menu = [
+    {
+        id: 1,
+        tittle: "beans and plantain",
+        category: "breakfast",
+        price: 15,
+        img: "./images/beans.jpg",
+        desc: "Nigerian Beans and plantain porridge is a meal made of ripe plantains and beans. This is a tasty dish and has become popular over the years, as most parents have discovered it is a good way to get their kids to eat beans. The addition of the ripe plantains gives the meal its sweet taste.",
+    },
+
+    {
+        id: 2,
+        tittle: "cocktails",
+        category: "drink",
+        price: 15,
+        img: "./images/drink 1.jpg",
+        desc: "Sex on the beach The classic Smirnoff Red Vodka & Archers mixed with orange and cranberry Juice for the perfect cocktail.",
+    },
+
+    {
+        id: 3,
+        tittle: "Egusi soup and Santana",
+        category: "breakfast",
+        price: 15,
+        img: "./images/egusi.jpg",
+        desc: "This soup is prepared with local melon seeds, and is prepared by most tribes in Nigeria in various ways. This West African cuisine is made with ground egusi seeds and different kinds of meat and fish.",
+    },
+
+    {
+        id: 4,
+        tittle: "Beer",
+        category: "drink",
+        price: 15,
+        img: "./images/heineken.jpg",
+        desc: "Heineken International, based in Holland, has produced this beer with 5% alcohol since 1873. The brewery used only malted barley, quality hops, special yeast, and purified water to create this beverage. It is popular in the US, Europe, and the Middle East.",
+    },
+
+    {
+        id: 5,
+        tittle: "Jollof Rice",
+        category: "breakfast",
+        price: 15,
+        img: "./images/jollof rice.jpg",
+        desc: "Jollof, or jollof rice, is a rice dish from West Africa. The dish is typically made with long-grain rice, tomatoes, onions, spices, vegetables and meat in a single pot, although its ingredients and preparation methods vary across different regions.",
+    },
+
+    {
+        id: 6,
+        tittle: "Tasty non alcholic drink",
+        category: "drink",
+        price: 15,
+        img: "./images/drink 2.jpg",
+        desc: "It’s never a bad idea to be more mindful about your drinking. That can be easier said than done if you’re at a bar with friends who are indulging in alcoholic beverages while you’re there.",
+    },
+
+    {
+        id: 7,
+        tittle: "Noodles",
+        category: "lunch",
+        price: 15,
+        img: "./images/noodles.jpg",
+        desc: "A bowl full of warm and freshly made noodles can turn your whole day upside down if you had been having a bad one.",
+    },
+
+    {
+        id: 8,
+        tittle: "Brown butter mushroom omelette",
+        category: "lunch",
+        price: 15,
+        img: "./images/images1.jpg",
+        desc: "Browning the butter before cooking your eggs helps give this omelette a rich, nutty flavour that helps bring the earthy ingredients to life. Thyme, chestnut mushrooms and spring onions are front and centre in this dish that's great for dinner, or even any time of the day.",
+    },
+
+    {
+        id: 9,
+        tittle: "Yam and egg sauce",
+        category: "breakfast",
+        price: 15,
+        img: "./images/yam.jpg",
+        desc: "Boiled yam and egg sauce is very easy to make.  Typically in Nigeria, we use tomatoes, scotch bonnet chilli and onions in making this sauce. Feel free to make this your own.",
+    },
+
+    {
+        id: 10,
+        tittle: "Jollof Rice and Plantain",
+        category: "lunch",
+        price: 15,
+        img: "./images/jollof&plantain.jpg",
+        desc: "Jollof rice and fried plantain a sweet combination.",
+    },
+
+];
+
+export default menu;
